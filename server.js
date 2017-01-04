3f958d8d29b6415fc89ad6b6b9ea230007470297
yuriolive <yuriso1994@gmail.com>
2017-01-04 19:52:09 -0200

    Heroku

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-
+/*
 app.use(function (req, res, next) {
 	if(!req.secure) {
 		next();
 	} else {
 		res.redirect('http://' + req.hostname + req.url);
 	}
-});
+});*/
 
 app.use(express.static('public'));
 
