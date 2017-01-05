a5022acab5fa5f5986f16f86ec32e8f2efbb8e95
yuriolive <yuriso1994@gmail.com>
2017-01-05 00:39:56 -0200

    Some fixes

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -8,14 +8,12 @@ module.exports = {
 		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
 		return axios.get(requestUrl).then(function(res) {
-			debugger;
 			if(res.data.cod && res.data.message) {
 				throw new Error(res.data.message);
 			} else {
 				return res.data.main.temp;
 			}
 		}, function(err) {
-			debugger;
 			throw new Error(err.response.data.message);
 		});
 	}
