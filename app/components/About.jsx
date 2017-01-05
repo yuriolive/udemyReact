11425238751f7d89165409b913006ddbe5a552b4
yuriolive <yuriso1994@gmail.com>
2017-01-05 17:36:45 -0200

    Style Navbar

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,13 @@
 var React = require('react');
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+	return (
+		<div>
+			<h3>About</h3>
+			<p>Welcome to about page!</p>
+		</div>
+	);
+
+};
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,13 @@
 var React = require('react');
 
-var Examples = (props) => <h3>Examples Component</h3>;
+var Examples = (props) => {
+	return (
+		<div>
+			<h3>Examples</h3>
+			<p>Welcome to examples page!</p>
+		</div>
+	);
+
+};
 
 module.exports = Examples;
